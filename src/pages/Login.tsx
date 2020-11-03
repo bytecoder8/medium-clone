f3d2411f6e282cf559a9fa512cba64f2041893f5
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-11-03 15:16:36 +0500

    Update Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 export function Login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (isSubmitting) {
+      return
+    }
+
+    setIsSubmitting(true)
+    axios(process.env.REACT_APP_API_BASE + '/users/login', {
+      method: 'POST',
+      data: {
+        user: {
+          email,
+          password
+        }
+      }
+    })
+    .then(console.log)
+    .catch(console.dir)
+    .finally(() => setIsSubmitting(false))
+  }
+
   return (
     <div className="login-page row">
-      <form className="col-md-5 mr-auto ml-auto">
+      <form className="col-md-5 mr-auto ml-auto" onSubmit={handleSubmit}>
         <h2>Sign In</h2>
         <div className="form-group">
           <label htmlFor="input-email">Email address</label>
           <input 
             type="email"
             name="email"
-            className="form-control"
             id="input-email"
+            className="form-control"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
         <div className="form-group">
@@ -19,12 +48,20 @@ export function Login() {
           <input
             type="password"
             name="password"
-            className="form-control"
             id="input-password"
+            className="form-control"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
         <div className="form-group">
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Signing in...' : 'Submit'}
+          </button>
         </div>
       </form>
     </div>
