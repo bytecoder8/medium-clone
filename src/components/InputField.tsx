6b62d521a5cef87db6ca569cd0a694c1ce11b76c
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-11-03 13:34:53 +0500

    Rename utility method

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,6 +1,6 @@
 import React, { InputHTMLAttributes } from 'react'
 import { useField } from 'formik'
-import { addClasses } from '../utils/form'
+import { addInputClasses } from '../utils/form'
 
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
@@ -9,7 +9,7 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
 
 export const InputField = (props: InputProps) => {
   const [field, {error, touched}] = useField(props)
-  const className: string = (props.className || '') + ' ' + addClasses(error, touched)
+  const className: string = (props.className || '') + ' ' + addInputClasses(error, touched)
 
   return(
     <>
--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,7 +1,7 @@
 import classNames from "classnames"
 
 
-export const addClasses = (error?: string, touched?: boolean) => {
+export const addInputClasses = (error?: string, touched?: boolean) => {
   return classNames({
     "is-invalid": touched && error,
     "is-valid": touched && !error
