ba504548e4d5435d6693bdfa2b2ff81ec0e5cc50
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-11-04 22:46:01 +0500

    Remove undefined in place of options, useCallback for doFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { ServerError } from '../types'
 
 
@@ -24,14 +24,14 @@ export function useFetch<T>(url: string): FetchHookResult<T> {
   const [isLoading, setIsLoading] = useState(false)
   const [data, setData] = useState()
   const [error, setError] = useState<ServerError>()
-  const [options, setOptions] = useState<FetchOptions>()
+  const [options, setOptions] = useState<FetchOptions>({})
 
   
-  const doFetch = (options?: FetchOptions) => {
+  const doFetch = useCallback((options: FetchOptions = {}) => {
     setError(undefined)
     setOptions(options)
     setIsLoading(true)
-  }
+  }, [])
   
   useEffect(() => {
     if (!isLoading) {
@@ -41,8 +41,8 @@ export function useFetch<T>(url: string): FetchHookResult<T> {
     const baseUrl = process.env.REACT_APP_API_BASE
     const cancelTokenSource = axios.CancelToken.source()
     axios(baseUrl + url, {
-      method: options?.method || 'GET',
-      data: options?.data || {}
+      method: options.method || 'GET',
+      data: options.data || {}
     })
     .then( res => {
       setData(res.data)
