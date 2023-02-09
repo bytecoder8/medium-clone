7fb24d615e1450573a0bd8cea55e72916cf8f2b8
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2023-02-09 11:02:27 +0500

    Add build settings

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import { Navbar } from './components/Navbar'
 import { CurrentUserProvider } from './context/currentUser'
 import { CurrentUserSetter } from './components/CurrentUserSetter'
 
+const baseUrl = import.meta.env.BASE_URL
 
 function App() {
   return (
     <div className="App">
       <CurrentUserProvider>
         <CurrentUserSetter>
-          <BrowserRouter>
+          <BrowserRouter basename={baseUrl}>
             <div className="container">
               <Navbar />
               <Routes />
